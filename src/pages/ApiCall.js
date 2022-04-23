d7ba29089b161ff1e85d3d424d08fbce416c8909
ddominiqUCSC <ddominiq@ucsc.edu>
2022-04-22 17:20:27 -0700

    framework for api call created, pings dummy api.

--- src/pages/ApiCall.js
+++ src/pages/ApiCall.js
@@ -1,9 +1,26 @@
+import axios from 'axios';
 import './apiCall.css';
 
 function ApiCall(){
+
+    const handleClick=()=> {
+
+        axios.get("https://reqres.in/api/users?page=2").then(response => {
+            console.log(response);
+
+        });
+        console.log("helloWorld");
+    }
     return(
 
-            <div>apiCallSite</div>
+            <div>apiCallSite
+                <button onClick={()=> {
+                    handleClick();
+
+                }}>Call Api</button>
+
+
+            </div>
     );
 }
 
