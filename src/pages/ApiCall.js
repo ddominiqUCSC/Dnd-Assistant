cf8f1c63f61276b5f3b16ca916e9687772057784
ddominiqUCSC <ddominiq@ucsc.edu>
2022-04-25 10:47:51 -0700

    Connected to DnD Api and generalized

--- src/pages/ApiCall.js
+++ src/pages/ApiCall.js
@@ -1,25 +1,46 @@
 import axios from 'axios';
 import './apiCall.css';
 
+
+/*
+
+    Boiler plate code for connecting to an API. 
+    Boiler plate includes both the handleClick function as well as the button format due to use of wrappers. 
+    Pings API and returns desired result
+
+    
+    Uses anymous function structure. Specify input inside the handle click portion. 
+
+
+            <button onClick={()=> {
+                    handleClick(input);  -> handleClick wrapped inside anymous function. 
+            }}>Call Api</button>
+
+    
+    To traverse the array structure of JSON use response.data.(subgroup)[index].(subgroup)etc etc
+
+*/
 function ApiCall(){
+    const handleClick=(input)=> {
+        axios.get("https://www.dnd5eapi.co/api"+input)
+        
+        .then(response => {
+            console.log(response.data.proficiencies[0].url);
+    
+        })
+        
+        .catch(err=>{
 
-    const handleClick=()=> {
-
-        axios.get("https://reqres.in/api/users?page=2").then(response => {
-            console.log(response);
-
+            console.log(err);
         });
-        console.log("helloWorld");
+
     }
     return(
 
             <div>apiCallSite
                 <button onClick={()=> {
-                    handleClick();
-
+                    handleClick("/classes/barbarian");
                 }}>Call Api</button>
-
-
             </div>
     );
 }
